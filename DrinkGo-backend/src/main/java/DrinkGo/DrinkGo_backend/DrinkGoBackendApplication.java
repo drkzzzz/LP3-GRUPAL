44dd2c51e945c7a051a7d3e5ae9b1246d43a8a80
motelo <cs.sisniegasar@unsm.edu.pe>
2026-01-24 13:16:16 -0500

    Initial commit

---
+++ DrinkGo-backend/src/main/java/DrinkGo/DrinkGo_backend/DrinkGoBackendApplication.java
@@ -0,0 +1,13 @@
+package DrinkGo.DrinkGo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkGoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkGoBackendApplication.class, args);
+	}
+
+}
---
+++ DrinkGo-backend/src/test/java/DrinkGo/DrinkGo_backend/DrinkGoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package DrinkGo.DrinkGo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkGoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
